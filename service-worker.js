7a8dcc94d7c84356ffc15d01fecefa494486acd1
FACRF <85409479+facrf@users.noreply.github.com>
2025-07-04 18:53:27 -0300

    Add files via upload
    teste

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "foto-mapa-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/exif-js/2.3.0/exif.min.js",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
